bb01788d0e28937bde1e1f83b7b9e94ca79723c6
Ivan Stefanov <ivanstefanov.it@gmail.com>
2023-11-03 15:09:59 +0200

    Initial Commit

---
+++ War-Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War-Game
+//
+//  Created by ivan.stefanov on 3.11.23.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
